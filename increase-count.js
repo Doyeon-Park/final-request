dcef6141ca2bbe8106d1743726fea71a663dd4b8
Doyeon Park (leah) <leah.park@momenti.tv>
2022-07-29 19:20:59 +0900

    add second-increase-count button

--- increase-count.js
+++ increase-count.js
@@ -1,5 +1,6 @@
 let myCounter = 0;
 const increaseCount = document.querySelector("#increase-count");
+increaseCount = document.querySelector("#second-increase-count");
 
 function addCounter() {
   increaseCount.textContent = "count is " + myCounter;
