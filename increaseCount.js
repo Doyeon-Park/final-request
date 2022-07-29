cb8f4fb34372fcaa7dddb931f3ed7ca7ffc02197
Doyeon Park (leah) <leah.park@momenti.tv>
2022-07-29 16:11:48 +0900

    modify js file details

--- increaseCount.js
+++ increaseCount.js
@@ -1,15 +1,15 @@
 let myCounter = 0;
 const increaseCount = document.querySelector("#increaseCount");
+/*html 문서에서 id를 찾아서 Element를 반환*/
 
 function addCounter() {
   increaseCount.textContent = "count is " + myCounter;
-}
+} /* addCounter 라는 함수를 생성 */
 
-document.onload = function () {
-  addCounter();
-};
+addCounter(); /* 함수 호출 */
 
 increaseCount.onclick = function () {
+  /*onclick을 이용하여 함수 실행*/
   myCounter++;
   addCounter();
 };
