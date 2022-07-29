85346052d982b5ade52f0c1a62de49260dd2a97b
Doyeon Park (leah) <leah.park@momenti.tv>
2022-07-29 17:45:24 +0900

    Remove comments

--- increaseCount.js
+++ increaseCount.js
@@ -1,15 +1,13 @@
 let myCounter = 0;
 const increaseCount = document.querySelector("#increaseCount");
-/*html 문서에서 id를 찾아서 Element를 반환*/
 
 function addCounter() {
   increaseCount.textContent = "count is " + myCounter;
-} /* addCounter 라는 함수를 생성 */
+} 
 
-addCounter(); /* 함수 호출 */
+addCounter(); /
 
 increaseCount.onclick = function () {
-  /*onclick을 이용하여 함수 실행*/
   myCounter++;
   addCounter();
 };
