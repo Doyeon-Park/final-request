0a1f8c66f1ae38c4472e55452214058154dc4013
Doyeon Park (leah) <leah.park@momenti.tv>
2022-08-01 23:45:24 +0900

    Rename one more time variable

--- increase-count.js
+++ increase-count.js
@@ -1,36 +1,31 @@
-let n1_Counter = 0;
-let n2_Counter = 0;
-let n3_Counter = 0;
+let CounterA = 0;
+let CounterB = 0;
+let CounterC = 0;
 
-//counter를 3개 넣고 일관성있게 통일해보자. 버튼도 3개로
-//id도 일관적이지 않고, kebab case 유지해야한다.
-//깔끔하게만 작성할 수 있는 방법에 대해서 고민.
+const amount_1 = document.querySelector("#countbox-1");
+const amount_2 = document.querySelector("#countbox-2");
+const amount_3 = document.querySelector("#countbox-3");
 
-const amt_1 = document.querySelector("#countbox_1");
-const amt_2 = document.querySelector("#countbox_2");
-const amt_3 = document.querySelector("#countbox_3");
-
-function add_number1() {
-  amt_1.textContent = "count is " + n1_Counter;
+function AddNumber1() {
+  amount_1.textContent = "count is " + CounterA;
 }
-function add_number2() {
-  amt_2.textContent = "count is " + n2_Counter;
+function AddNumber2() {
+  amount_2.textContent = "count is " + CounterB;
 }
-function add_number3() {
-  amt_3.textContent = "count is " + n3_Counter;
+function AddNumber3() {
+  amount_3.textContent = "count is " + CounterC;
 }
 
-countbox_1.onclick = function () {
-  n1_Counter++;
-  add_number1();
+amount_1.onclick = function () {
+  CounterA++;
+  AddNumber1();
+};
+amount_2.onclick = function () {
+  CounterB++;
+  AddNumber2();
 };
 
-countbox_2.onclick = function () {
-  n2_Counter++;
-  add_number2();
-};
-
-countbox_3.onclick = function () {
-  n3_Counter++;
-  add_number3();
+amount_3.onclick = function () {
+  CounterC++;
+  AddNumber3();
 };
