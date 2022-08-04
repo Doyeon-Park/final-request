c4f9d35feb8089a7e3053e2e2e638aa1ed99d262
Doyeon Park (leah) <leah.park@momenti.tv>
2022-08-04 11:25:36 +0900

    Rename four more times variable

--- increase-count.js
+++ increase-count.js
@@ -2,30 +2,30 @@ let count1 = 0;
 let count2 = 0;
 let count3 = 0;
 
-const amountButton1 = document.querySelector("#count-button1");
-const amountButton2 = document.querySelector("#count-button2");
-const amountButton3 = document.querySelector("#count-button3");
+const countButton1 = document.querySelector("#count-button1");
+const countButton2 = document.querySelector("#count-button2");
+const countButton3 = document.querySelector("#count-button3");
 
 function increaseCount1() {
-  amountButton1.textContent = "count is " + count1;
+  countButton1.textContent = "count is " + count1;
 }
 function increaseCount2() {
-  amountButton2.textContent = "count is " + count2;
+  countButton2.textContent = "count is " + count2;
 }
 function increaseCount3() {
-  amountButton3.textContent = "count is " + count3;
+  countButton3.textContent = "count is " + count3;
 }
 
-amountButton1.onclick = function () {
+countButton1.onclick = function () {
   count1++;
   increaseCount1();
 };
-amountButton2.onclick = function () {
+countButton2.onclick = function () {
   count2++;
   increaseCount2();
 };
 
-amountButton3.onclick = function () {
+countButton3.onclick = function () {
   count3++;
   increaseCount3();
 };
