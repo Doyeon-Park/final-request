57ef75a9ec4b8abb00d58bb4d8b55adb18fa0c52
Doyeon-Park <64131469+Parkdo-yeon@users.noreply.github.com>
2022-08-04 17:57:17 +0900

    Implement counters by using array (#5)
    * Implement array counters
    
    * Implement by using class
    
    Co-authored-by: Doyeon Park (leah) <leah.park@momenti.tv>

--- increase-count.js
+++ increase-count.js
@@ -1,31 +1,10 @@
-let count1 = 0;
-let count2 = 0;
-let count3 = 0;
+let values = [];
+const countButtons = document.querySelectorAll(".count-button");
 
-const countButton1 = document.querySelector("#count-button1");
-const countButton2 = document.querySelector("#count-button2");
-const countButton3 = document.querySelector("#count-button3");
+for (let i = 0; i < countButtons.length; i++) {
+  values.push(0);
 
-function increaseCount1() {
-  countButton1.textContent = "count is " + count1;
+  countButtons[i].onclick = function () {
+    countButtons[i].textContent = `count is ${++values[i]}`;
+  };
 }
-function increaseCount2() {
-  countButton2.textContent = "count is " + count2;
-}
-function increaseCount3() {
-  countButton3.textContent = "count is " + count3;
-}
-
-countButton1.onclick = function () {
-  count1++;
-  increaseCount1();
-};
-countButton2.onclick = function () {
-  count2++;
-  increaseCount2();
-};
-
-countButton3.onclick = function () {
-  count3++;
-  increaseCount3();
-};
