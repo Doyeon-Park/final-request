18b8348a84eb201b8183a43280cfeb03e026e943
Doyeon Park (leah) <leah.park@momenti.tv>
2022-08-04 17:49:08 +0900

    Implement array counters

--- increase-count.js
+++ increase-count.js
@@ -1,31 +1,14 @@
-let count1 = 0;
-let count2 = 0;
-let count3 = 0;
+const COUNT = 5;
 
-const countButton1 = document.querySelector("#count-button1");
-const countButton2 = document.querySelector("#count-button2");
-const countButton3 = document.querySelector("#count-button3");
+let countButtons = [];
+let values = [];
 
-function increaseCount1() {
-  countButton1.textContent = "count is " + count1;
+for (let i = 0; i < COUNT; i++) {
+  values.push(0);
+  const button = document.querySelector("#count-button-" + (i + 1));
+  button.onclick = function () {
+    button.textContent = `count is ${++values[i]}`;
+  };
+
+  countButtons.push(button);
 }
-function increaseCount2() {
-  countButton2.textContent = "count is " + count2;
-}
-function increaseCount3() {
-  countButton3.textContent = "count is " + count3;
-}
-
-countButton1.onclick = function () {
-  count1++;
-  increaseCount1();
-};
-countButton2.onclick = function () {
-  count2++;
-  increaseCount2();
-};
-
-countButton3.onclick = function () {
-  count3++;
-  increaseCount3();
-};
